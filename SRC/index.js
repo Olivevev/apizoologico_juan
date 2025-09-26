7681f05f98f927288c6e800d8991406838d15ba8
Olive <Olive.gmail.com>
2025-09-25 20:45:39 -0500

    Peticion POST

--- SRC/index.js
+++ SRC/index.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-    res.send('¡Hola Mundo!')
-})
-
+const parser = require("body-parser");
+const express = require('express');
+const app = express();
+const port = 3000;
+const animalRoutes = require("./Routes/Animal");
+const mongoose = require("mongoose");
+require('dotenv').config();
+app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
+app.use(parser.json()); // transforma los datos a formato JSON
+//Gestión de las rutas usando el middleware
+app.use("/api", animalRoutes);
+app.use(express.json());
+//Conexión a la base de datos
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Conexión exitosa"))
+    .catch((error) => console.log(error));
+//Conexión al puerto
 app.listen(port, () => {
-    console.log('La Aplicacion Se Esta Ejecutando Por El Puerto'+ `${port}`)
-})
+    console.log(`Example app listening on port ${port}`)
+});
